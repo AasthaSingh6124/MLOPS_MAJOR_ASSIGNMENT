d5b9204fa4b94452b27bee43187f14776d835bda
Aastha Singh <aasthasingh.t@gmail.com>
2025-11-12 16:11:39 +0000

    Final workflow

--- train.py
+++ train.py
@@ -12,17 +12,18 @@ def main():
     X_flat = X.reshape((n_samples, -1))
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X_flat, y, train_size=0.7, random_state=42, stratify=y)
+        X_flat, y, train_size=0.7, random_state=42, stratify=y
+    )
 
-<<<<<<< Updated upstream
-    clf = DecisionTreeClassifier(random_state=42)
-=======
     clf = DecisionTreeClassifier(max_depth=20, random_state=42)
->>>>>>> Stashed changes
     clf.fit(X_train, y_train)
+
+    train_acc = clf.score(X_train, y_train)
+    print(f"✅ Training accuracy: {train_acc:.4f}")
 
     joblib.dump({'model': clf, 'X_test': X_test, 'y_test': y_test}, 'savedmodel.pth')
     print("✅ Training completed. Model saved as savedmodel.pth")
 
 if __name__ == "__main__":
     main()
+
